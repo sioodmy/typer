1a28c0e8723ce6319ce5f85530e0a925200d7d9b
GrajAP <adampisarczyk2006@gmail.com>
2022-11-27 18:28:12 +0100

    starcik

---
+++ docs.md
@@ -0,0 +1,6 @@
+#Zasady
+- nazwy folderów, komponentów pierwsza wielka litera
+- commity na testing,
+- sensowne commity (nazwy i rozmiar),
+- nie wchodzimy na swoje kompetencje i nie przeszkadzamy sobie nawzajem,
+- mniej = lepiej,
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pl">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" type="image/svg+xml" href="./src/Img/favicon-dark.png" media="(prefers-color-scheme: light)" />
+  <link rel="icon" type="image/svg+xml" href="./src/Img/favicon-light.png" media="(prefers-color-scheme: dark)" />
+
+    <title>Typer</title>
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="module" src="/src/main.jsx"></script>
+  </body>
+</html>
